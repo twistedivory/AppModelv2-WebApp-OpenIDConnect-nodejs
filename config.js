1172171852b265a7ebae898197ad9edd984affaf
Brandon Werner <brandwe@microsoft.com>
2015-08-27 18:55:34 -0700

    no message

--- config.js
+++ config.js
@@ -5,5 +5,10 @@
  	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
  	realm: 'http://localhost:3000',
  	clientID: '<Your App ID>',
- 	clientSecret: '<Your client secret>'
+ 	clientSecret: '<Your client secret>',
+ 	skipUserProfile: true, // for OpenID only flows this should be set to true
+ 	responseType: 'id_token', // for login only flows
+ 	responseMode: 'form_post', // As per the OAuth 2.0 standard.
+ 	//scope: ['email', 'profile'] // additional scopes you may wish to pass
+
  };
